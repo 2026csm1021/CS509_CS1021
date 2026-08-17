8355f8cf202c6a0714ac74d52a31580e18c600d8
2026csm1021 <2026csm1021@iitrpr.ac.in>
2026-08-17 22:37:49 +0530

    Adding graph.h and input.txt

---
+++ Assignment 3/graph.h
@@ -0,0 +1,20 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Edge {
+    int neighbour;
+    int weight;
+};
+
+struct Graph {
+    int vertices;
+    int Edges;
+
+    vector<vector<Edge>> adjacency;
+};
+Graph readGraph(const string& filename);
+
+#endif
